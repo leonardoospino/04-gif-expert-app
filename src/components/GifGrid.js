f9a46b5c2f1fbbf5edde5141e18dbd2d45019be3
Leonardo F. Ospino Ayala <leonardo.ospino.ayala@gmail.com>
2021-05-03 14:23:54 -0500

    get gifs helper

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({category}) => {
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=N4Bf46QstVAWfFAO7X19XK9mLOaB7WDI&limit=10&q=Matrix`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+  const [images, setImages] = useState([]);
 
-    const gifs = data.map(img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    });
-
-    console.log(gifs);
-  };
-
-  getGifs();
+  useEffect(() => {
+    getGifs(category).then(setImages);
+  }, []);
 
   return (
     <>
       <h3>{ category }</h3>
+      <div className="card-grid">
+          {
+            images.map(img => (
+              <GifGridItem key={ img.id } { ...img } />
+            ))
+          }
+      </div>
     </>
   )
 }
